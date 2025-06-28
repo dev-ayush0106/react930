a0fb676d426c30751fc776d534331c43b35b7f3e
ayush <ayushshivoy06@gmail.com>
2025-06-28 11:59:04 +0530

    Todo

--- src/Class 6/TodoItem.jsx
+++ src/Class 6/TodoItem.jsx
@@ -4,7 +4,7 @@ const TodoItem = ({todo,toggle,remove}) => {
   return (
     <div>
       <li className={todo.completed?"completed":""}>
-        <span onClick={toggle(todo.id)}>{todo.task}</span>
+        <span onClick={()=>toggle(todo.id)}>{todo.task}</span>
         <button onClick={()=>{remove(todo.id)}}>Remove</button>
       </li>
     </div>
